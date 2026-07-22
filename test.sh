ae0337b937049dfd5649f248b5aa434be5299749
bekkayya <153224752+bekkayya@users.noreply.github.com>
2026-07-22 23:40:49 +0000

    Delete test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf build
-
-./gradlew build jar
-
-mv build/libs/create_fly_wheels-0.1.jar /home/bekka/.local/share/PrismLauncher/instances/CrissyCraft_reactive_stress_test/minecraft/mods/
